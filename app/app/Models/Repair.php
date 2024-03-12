3c1f355d691162c4bc9ab024d470d7f2cec2fbe3
Ezama Richard Icytrey <icytreyrichards@yahoo.com>
2024-03-12 12:16:46 +0300

    making sure everything is the same
    final

--- app/app/Models/Repair.php
+++ app/app/Models/Repair.php
@@ -8,54 +8,52 @@ class Repair extends Model
     use HasFactory;
     protected $guarded = ['id'];
     public function assigned(){
-        return $this->belongsTo(Admin::class, 'engineer_assigned');
+        return $this->hasOne(Admin::class,'id', 'engineer_assigned');
     }
 
     public function preparedby(){
-        return $this->belongsTo(Admin::class, 'created_by');
+        return $this->hasOne(Admin::class,'id', 'created_by');
     }
 
     
     public function operator(){
-        return $this->belongsTo(Admin::class, 'delivered_by');
+        return $this->hasOne(Admin::class,'id', 'delivered_by');
     }
     public function equipment(){
-        return $this->belongsTo(Vehicle::class, 'vehicle_id');
+        return $this->hasOne(Vehicle::class,'id', 'vehicle_id');
     }
 
     public function diognised(){
-        return $this->hasOne(Admin::class,"id",'diognised_by');
+        return $this->hasOne(Admin::class,'id', 'diognised_by');
     }
 
     
     public function completedby(){
-        return $this->hasOne(Admin::class, "id",'completed_by');
+        return $this->hasOne(Admin::class,'id', 'completed_by');
     }
 
     
     
     public function issuedby(){
-        return $this->belongsTo(Admin::class, 'issued_by');
+        return $this->hasOne(Admin::class,'id', 'issued_by');
     }
 
 
     public function testedBy(){
-        return $this->belongsTo(Admin::class, 'tested_by');
+        return $this->hasOne(Admin::class,'id', 'tested_by');
     }
 
     public function VerifiedBy(){
-        return $this->belongsTo(Admin::class, 'certified_by');
+        return $this->hasOne(Admin::class,'id', 'certified_by');
     }
 
+
     public function statuses(){
-        return $this->belongsTo(Status::class, 'status');
+        return $this->hasOne(Status::class,'id', 'status');
     }
 
     public function persons_assigned(){
-        return $this->hasmany(Assigned::class, "repair_id","id");
+        return $this->hasMany(Assigned::class, "repair_id","id");
     }
-
-
-    
 
 }
